fa6eca72bd7cf14b6b416ebc3ec8f6c9b2caf5bf
C Shen <chuishen@gmail.com>
2022-10-03 10:16:09 +0800

    Update get-latestPackages.ps1

--- get-latestPackages.ps1
+++ get-latestPackages.ps1
@@ -2,7 +2,7 @@ $configPath = "c:\config"
 if (Test-Path -Path "$configPath" -PathType Container) {
     Write-Output "$configPath exists"
 }
-else{
+else {
     New-Item -Path $configPath -ItemType Directory -Force
 }
 Write-Output "Download latest workstaion config from github..."
