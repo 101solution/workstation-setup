65232e146a44778fcb3f634e11a26cae1bcaa43d
C Shen <chuishen@gmail.com>
2022-12-22 16:04:04 +0800

    display version

--- get-latestPackages.ps1
+++ get-latestPackages.ps1
@@ -5,10 +5,14 @@ if (Test-Path -Path "$configPath" -PathType Container) {
 else {
     New-Item -Path $configPath -ItemType Directory -Force
 }
-Write-Output "Download latest workstaion config from github..."
-$githubRepoUrl = "https://api.github.com/repos/101solution/workstation-setup/tags"
+
+$githubRepoUrl = "https://api.github.com/repos/101solution/workstation-setup/releases"
+Write-Output "Download latest release from github $githubRepoUrl..."
 $tags = Invoke-RestMethod -Uri $githubRepoUrl -ErrorAction SilentlyContinue
-$zipUrl = ($tags | Select-Object -first 1).zipball_url
+$latestRelease = ($tags | Where-Object {(-not $_.draft) -and (-not $_.prerelase)} | Select-Object -first 1)
+$zipUrl = $latestRelease.zipball_url
+$version = $latestRelease.tag_name
+Write-Output "Latest version is $version"
 Invoke-RestMethod -Uri $zipUrl -OutFile "$configPath\workstation.zip"
 #Using .Net class System.IO.Compression.ZipFile
 Add-Type -Assembly "System.IO.Compression.Filesystem"
